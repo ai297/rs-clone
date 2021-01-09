8f5cbaae9e084313de581c3b9bd28474ef39bb3a
Anna ZAS <ansiv@yandex.ru>
2021-01-09 19:39:05 +0300

    feat: add players list component

--- src/client/components/player-list/player-list.ts
+++ src/client/components/player-list/player-list.ts
@@ -1,59 +1,31 @@
 import { BaseComponent } from '../base-component';
 import { CSSClasses } from '../../enums';
-
-// предлагаю это вынести в отдельный класс, куда передавать объект с параметрами или енам
-function createElement(
-  tag: string,
-  cssClass: CSSClasses,
-  title?: string,
-  attr?: string,
-): HTMLElement {
-  const element = document.createElement(tag);
-
-  element.classList.add(cssClass);
-  element.innerText = title;
-  element.setAttribute('src', attr); // переделать надо нормально
-
-  return element;
-}
-
-// хардкод!!!
-const playerList = [
-  {
-    avatar: 'player1Ava',
-    name: 'player1Name',
-  },
-  {
-    avatar: 'player2Ava',
-    name: 'player2Name',
-  },
-];
+import { PlayerListItem } from '../player-list-item/player-list-item';
 
 export class PlayerList extends BaseComponent {
   constructor() {
-    super();
-    this.container.classList.add(CSSClasses.playerListContainer);
-
-    // если планируем локализацию, то надо в папку компонента класть объект с подписями, лучше это сразу делать
-    const playerListTitle = createElement('div', CSSClasses.playerListTitle, 'Список игроков');
-    const playerListWrapper = createElement('ul', CSSClasses.playerListWrapper);
-
-    playerList.forEach((player) => {
-      // это предлагаю в отдельный класс
-      const playerListItem = createElement('li', CSSClasses.playerListItem);
-      const playerAvatar = createElement('img', CSSClasses.playerAvatar, '', player.avatar);
-      const playerName = createElement('span', CSSClasses.playerName, player.name);
-      // сюда наверное добавятся еще эл-ты с полями объекта player
-
-      playerListItem.append(playerAvatar, playerName);
-      playerListWrapper.append(playerListItem);
-    });
-
-    // предлагаю кнопки вынести в отдельный класс
-    const startGameBtn = createElement('button', CSSClasses.startGameBtn, 'Начать игру');
-
-    this.container.append(playerListTitle, playerListWrapper, startGameBtn);
+    super(CSSClasses.PlayerList);
   }
 
-  /* methods of class */
+  addPlayer(name: string, hero: string, avatar: string): HTMLElement {
+    const newPlayer: PlayerListItem = new PlayerListItem(name, hero, avatar);
+    this.element.append(newPlayer.element);
+
+    return this.element;
+  }
+
+  removePlayer(name: string): void {
+    const players = this.element.querySelectorAll(`.${CSSClasses.PlayerName}`);
+    let removedPlayer: HTMLElement | null = null;
+
+    players.forEach((player) => {
+      if (player.textContent === name) {
+        removedPlayer = player.closest(`.${CSSClasses.PlayerListItem}`);
+      }
+    });
+
+    if (removedPlayer) {
+      (removedPlayer as HTMLElement).remove();
+    }
+  }
 }
--- src/client/enums/css-classes.ts
+++ src/client/enums/css-classes.ts
@@ -2,11 +2,10 @@ export enum CSSClasses {
   Component = 'component',
   StartScreen = 'start-screen',
   StartScreenButton = 'start-screen__button',
-  playerListContainer = 'player-list',
-  playerListTitle = 'player-list__title',
-  playerListWrapper = 'player-list__wrapper',
-  playerListItem = 'player-list__item',
-  playerAvatar = 'player__avatar',
-  playerName = 'player__name',
-  startGameBtn = 'start-game-btn',
+
+  PlayerList = 'player-list',
+  PlayerListItem = 'player-list__item',
+  PlayerAvatar = 'player__avatar',
+  PlayerName = 'player__name',
+  PlayerHero = 'player__hero',
 }
