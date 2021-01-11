dd2e894dae331f3c5b7f12bc841650a769346c0e
Kris Casper <sw.darkwind@gmail.com>
2021-01-11 15:16:03 +0300

    feat: add lifecicle methods signature to IComponent

--- src/client/components/base-component.ts
+++ src/client/components/base-component.ts
@@ -2,6 +2,10 @@ import { CSSClasses } from '../enums';
 
 export interface IComponent {
   readonly element: HTMLElement;
+  beforeAppend?: () => Promise<void>;
+  onAppended?: () => Promise<void>;
+  beforeRemove?: () => Promise<void>;
+  onRemoved?: () => Promise<void>;
 }
 
 export abstract class BaseComponent implements IComponent {
