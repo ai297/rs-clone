573fbb25ae08ca4e59bfda9ef73a1f84d01047f6
Kris Casper <sw.darkwind@gmail.com>
2021-01-13 03:36:41 +0300

    feat: add selectSpell method to game-service (client-side) and players modified callbacks

--- src/client/services/game-service.ts
+++ src/client/services/game-service.ts
@@ -6,6 +6,7 @@ import {
   IPlayerInfo,
   ICreatePlayerRequest,
   ISpellSelected,
+  IHealthUpdate,
 } from '../../common';
 import { ServerConnection } from './server-connection';
 
@@ -19,10 +20,8 @@ export class GameService {
   constructor(private readonly connection: ServerConnection) {
     connection.addEventListener(HubEventsClient.AddPlayer, (player: IPlayerInfo) => this.addPlayer(player));
     connection.addEventListener(HubEventsClient.RemovePlayer, (playerId: string) => this.removePlayer(playerId));
-    connection.addEventListener(HubEventsClient.SpellSelected, (message: ISpellSelected) => {
-      if (this.onPlayerSelectSpell) this.onPlayerSelectSpell(message.playerId, message.spellCards);
-      return HubResponse.Ok();
-    });
+    connection.addEventListener(HubEventsClient.SpellSelected, (message: ISpellSelected) => this.spellSelect(message));
+    connection.addEventListener(HubEventsClient.UpdateHealath, (message: IHealthUpdate) => this.healthUpdate(message));
   }
 
   get currentGameId(): string { return this.gameId; }
@@ -55,15 +54,32 @@ export class GameService {
     return HubResponse.Ok();
   }
 
+  private async spellSelect(message: ISpellSelected): Promise<IHubResponse<null>> {
+    if (this.onPlayerSelectSpell) await this.onPlayerSelectSpell(message.playerId, message.spellCards);
+    return HubResponse.Ok();
+  }
+
+  private async healthUpdate(message: IHealthUpdate): Promise<IHubResponse<null>> {
+    const player: IPlayerInfo = this.currentPlayers.find((playerInfo) => playerInfo.id === message.playerId);
+    if (player) player.health = message.currentHealth;
+    if (message.isDamage && this.onPlayerTakeDamage) {
+      await this.onPlayerTakeDamage(message.playerId, message.currentHealth);
+    }
+    if (!message.isDamage && this.onPlayerTakeHeal) {
+      await this.onPlayerTakeHeal(message.playerId, message.currentHealth);
+    }
+    return HubResponse.Ok();
+  }
+
   onPlayerJoined?: (playerInfo: IPlayerInfo) => void;
 
   onPlayerLeaved?: (playerId: string) => void;
 
-  onPlayerSelectSpell?: (playerId: string, cardsInSpell: number) => void;
+  onPlayerSelectSpell?: (playerId: string, cardsInSpell: number) => Promise<void>;
 
-  onPlayerTakeDamage?: (playerId: string, damage: number) => Promise<void>;
+  onPlayerTakeDamage?: (playerId: string, currentHealth: number) => Promise<void>;
 
-  onPlayerTakeHeal?: (playerId: string, heal: number) => Promise<void>;
+  onPlayerTakeHeal?: (playerId: string, currentHealth: number) => Promise<void>;
 
   /**
    * This method returns promise, that resolve with gameId: string as argument
@@ -98,5 +114,9 @@ export class GameService {
     const playerInfo = await this.connection.dispatch<IPlayerInfo>(HubEventsServer.AddPlayer, request);
     this.playerId = playerInfo.id;
     return playerInfo;
+  }
+
+  async selectSpell(cardIds: Array<string>): Promise<void> {
+    await this.connection.dispatch(HubEventsServer.SelectSpell, cardIds);
   }
 }
