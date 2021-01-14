c59c2af99530adf823f7e5cca24a2e719ecfded9
Kris Casper <sw.darkwind@gmail.com>
2021-01-14 23:02:54 +0300

    fix: naming & type errors

--- src/server/connection/index.ts
+++ src/server/connection/index.ts
@@ -1,4 +1,4 @@
 export { ClientConnection } from './client-connection';
 export { ConnectionService } from './connection-service';
 export { ConnectionEvents } from './connection-events';
-export { createFackeConnection } from './fake-connection';
+export { createFakeConnection } from './fake-connection';
