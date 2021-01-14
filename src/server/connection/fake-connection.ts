3bce0af9bfe1981b717368c789b4f5f5988963ff
Kris Casper <sw.darkwind@gmail.com>
2021-01-14 16:09:10 +0300

    fix: typo

--- src/server/connection/fake-connection.ts
+++ src/server/connection/fake-connection.ts
@@ -2,7 +2,7 @@ import { ClientConnection } from './client-connection';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export function createFackeConnection(): ClientConnection {
+export function createFakeConnection(): ClientConnection {
   return new ClientConnection({
     id: Math.random().toFixed(10),
     on: () => { },
