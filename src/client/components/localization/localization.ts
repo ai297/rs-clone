061ab9bac11b10b5866d13233e093922d1a4763d
Tamara <yaroslavcevatamara@gmail.com>
2021-01-15 14:03:32 +0300

    feat: add start screen localization

--- src/client/components/localization/localization.ts
+++ src/client/components/localization/localization.ts
@@ -1,4 +1,5 @@
 import { ILobbyLocalization } from './lobby-localization';
+import { IStartScreenLocalization } from './start-screen-localization';
 
 export const LOBBY_DEFAULT_LOCALIZATION: ILobbyLocalization = {
   EnterYourName: 'Введите имя',
@@ -7,4 +8,11 @@ export const LOBBY_DEFAULT_LOCALIZATION: ILobbyLocalization = {
   GameLink: 'Ссылка на присоединение к игре',
   GameId: 'Ключ игры',
   PlayerList: 'Список игроков',
+};
+
+export const START_SCREEN_DEFAULT_LOCALIZATION: IStartScreenLocalization = {
+  NewGame: 'Новая игра',
+  Join: 'Присоединиться',
+  Rules: 'Правила',
+  Settings: 'Настройки',
 };
