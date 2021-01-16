078da9d54840d3bf0e6ea20fa64d3255b5aae13d
Kris Casper <57856125+ai297@users.noreply.github.com>
2021-01-16 12:40:39 +0300

    Add callbacks to game-service for game-screen (#50)
    * feat: add callbacks to game-service for game-screen
    
    * fix: callback names

--- src/common/interfaces/DTO/index.ts
+++ src/common/interfaces/DTO/index.ts
@@ -2,3 +2,4 @@ export { IHubResponse } from './hub-response';
 export { ICreatePlayerRequest } from './create-player-request';
 export { IHealthUpdate } from './update-heath-message';
 export { ISpellSelected } from './spell-selected-message';
+export { IDiceRoll } from './roll-dice-message';
