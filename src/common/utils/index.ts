1bf45883f35501a2a9705b72c88ce6cf4d0d7b7c
SonKn1ght <maximiilian321@gmail.com>
2021-01-26 11:28:08 +0300

    feat: play sound function

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -3,3 +3,4 @@ export { createElement } from './create-element';
 export { delay } from './delay';
 export { forEachAsync } from './foreach-async';
 export { getRandomInteger } from './random-integer';
+export { playSound } from './play-sound';
---
+++ src/common/utils/play-sound.ts
@@ -0,0 +1,14 @@
+import { Sounds } from '../enums';
+
+export const playSound = async (sound: Sounds): Promise<void> => new Promise((resolve) => {
+  const audio = new Audio(`./${sound}`);
+
+  audio.addEventListener('canplaythrough', () => {
+    audio.play();
+  });
+
+  audio.addEventListener('ended', () => {
+    console.log('rock-and-roll');
+    resolve();
+  });
+});
