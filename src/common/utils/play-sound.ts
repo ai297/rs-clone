38cb676f860f69ea86411ce81f917d1577df5b5f
SonKn1ght <maximiilian321@gmail.com>
2021-01-26 14:14:15 +0300

    fix: path sound

--- src/common/utils/play-sound.ts
+++ src/common/utils/play-sound.ts
@@ -1,7 +1,7 @@
 import { Sounds } from '../enums';
 
 export const playSound = (sound: Sounds): Promise<void> => new Promise((resolve) => {
-  const audio = new Audio(`./${sound}`);
+  const audio = new Audio(`./sounds/${sound}`);
 
   audio.addEventListener('canplaythrough', () => {
     audio.play();
