2204f62e5a7ad897f41721be450beb898f270af2
SonKn1ght <maximiilian321@gmail.com>
2021-01-29 13:28:46 +0300

    fix: path

--- public/path.ts
+++
@@ -1,3 +0,0 @@
-import path from 'path';
-
-export const PUBLIC_PATH = path.resolve(__dirname, './');
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import path from 'path';
 import { createServer } from 'http';
 import App from './app';
-import { PUBLIC_PATH } from '../../public/path';
 
 const PORT = process.env.PORT || 3000;
+const PATH = process.env.PUBLIC_PATH;
+const PUBLIC_PATH = PATH ? path.resolve(__dirname, PATH) : path.resolve(__dirname, './public');
 
 const exp = express();
 const server = createServer(exp);
