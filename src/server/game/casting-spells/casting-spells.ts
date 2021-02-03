383db54dac95e49c0d7094d63abb04b768124190
Kris Casper <sw.darkwind@gmail.com>
2021-02-03 19:05:52 +0300

    fix: spell casting process

--- src/server/game/casting-spells/casting-spells.ts
+++ src/server/game/casting-spells/casting-spells.ts
@@ -17,35 +17,41 @@ export class CastingSpells {
     const queue: Array<Player> = this.players.slice().sort((a, b) => b.spell.initiative - a.spell.initiative);
     // TODO: пофиксить одинаковую инициативу
 
-    await forEachAsync(queue, async (player, index, breakCast) => {
-      if (!player.isAlive) return;
+    await forEachAsync(queue, async (currentPlayer, index, breakCast) => {
+      if (!currentPlayer.isAlive) return;
 
-      const cards: Array<ICard> = await player.startSpellCasting();
-      const positionPlayer: number = this.players.findIndex((current) => player === current);
+      const cards: Array<ICard> = await currentPlayer.startSpellCasting();
+      const positionPlayer: number = this.players.findIndex((player) => player === currentPlayer);
 
       await forEachAsync(cards, async (currentCard: ICard, cardIndex, breakSpell) => {
+        if (!currentPlayer.isAlive) {
+          breakSpell();
+          return;
+        }
         // console.log(currentCard.id);
-        await player.castCard(currentCard);
+        await currentPlayer.castCard(currentCard);
 
         const handler = this.spells.getHandler(currentCard.id);
         await handler(positionPlayer, currentCard);
         // this.players.forEach((cur, index) => console.log('pozit player', index, 'hit point', cur.hitPoints));
 
-        const deadThisCast: Array<Player> = this.players.filter((current: Player) => !current.isAlive);
-        // если есть покойничек то запускаем его отработку
-        if (deadThisCast.length > 0) {
-          // обновляем состояние списка игроков
-          this.players = this.players.filter((current: Player) => current.isAlive);
-          // потрошим с покойничка карты и отдаем их в отбой
-          this.removeCardsFromCorpse(deadThisCast);
-          // проверяем игру на законченность
-          if (this.checkForEndGame()) {
-            breakSpell();
-            breakCast();
-          }
+        if (this.checkForEndGame()) {
+          breakSpell();
+          breakCast();
         }
       });
 
+      const deadThisCast: Array<Player> = this.players.filter((current: Player) => !current.isAlive);
+      // если есть покойничек то запускаем его отработку
+      if (deadThisCast.length > 0) {
+        // обновляем состояние списка игроков
+        this.players = this.players.filter((current: Player) => current.isAlive);
+        // потрошим с покойничка карты и отдаем их в отбой
+        this.removeCardsFromCorpse(deadThisCast);
+      }
+    });
+
+    this.players.forEach((player) => {
       const usedCards: Array<ICard> = player.transferSpellCards();
       this.game.usedCardHandler(usedCards);
     });
